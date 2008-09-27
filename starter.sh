906d4e639f549c9ad699ff7a9d2086366bb6fab0
Brian Bockelman <bbockelm@cse.unl.edu>
2008-09-27 18:18:07 +0000

    Pacman updates for GridFTP_HDFS
    git-svn-id: svn://t2.unl.edu/brian/gridftp_hdfs@2291 1fda6f16-4416-0410-bfd6-867a04880151

--- starter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh                                                            
-                                                                      
-. VDT_LOCATION/setup.sh
-
-echo $CLASSPATH
-
-exec $GLOBUS_LOCATION/sbin/globus-gridftp-server -c gridftp.conf -dsi hdfs -no-fork
-
